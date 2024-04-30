8c559d9e065acdf2ec3372c7fb82d41f34bbe88b
Jarred Lechner <jarred.lechner@mni.thm.de>
2024-04-30 15:16:36 +0200

    first working Implementation of tangle and weave

--- skills/Text/Literate.java
+++ skills/Text/Literate.java
@@ -5,30 +5,90 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Literate {
-    static boolean isText = false;
+   
     static String tangle(String path) {
-        return tangle(path, "//<<program>>");
-    }
-    static String tangle(String path, String label){
         StringBuilder finalCode = new StringBuilder();
-        String content = Text.cutOut(path, label);
+        List<String> text = new ArrayList<>();
+        try{ text = Files.readAllLines(Path.of(path));
+        }catch(Exception e) {
+            System.err.println("Error reading file");
+            System.exit(1);
+        }
+        boolean skip = false;
+        for(String line : text) {
+            if (Syntax.isText(line)) {
+                skip = true;
+                
+            }
+            
+            if (Syntax.isTextEnd(line)) {
+                skip = false;
+                continue;
+            }
+            if(Syntax.isLabel(line)){
+                skip = true;
+            }
+            if(Syntax.isEndLabel(line)){
+                skip = false;
+                continue;
+            }
+            if (skip) {
+                continue;
+            }
+            if(Syntax.isLabelDeclaration(line)){
+                finalCode.append(replaceLabel(Syntax.useLabel(line),Path.of(path)));
+                continue;
+            }
+            finalCode.append(line).append("  \n");
+        }
+        return finalCode.toString();
+
+    }
+    static String replaceLabel( String label, Path path){
+        StringBuilder finalCode = new StringBuilder();
+        String content = extractMacro(label, path);
+        
         String[] lines = content.split("\\n");
         boolean isText = false;
-        boolean wasText = false;
-        for( String line : lines) {
-             wasText = isText;
-            isText = isText(line);
-            if(isText || wasText) continue;
-            if(isLabelDeclaration(line)) {
-                line = line.replace("<<<", "<<").replace(">>>", ">>");
-                finalCode.append(tangle(path,line));
-            }
-            else if(!isLabel(line)&& !isLabelDeclaration(line)) {
-                finalCode.append(line).append("\n");
+        for (String line : lines) {
+           
+           
+            
+            if (Syntax.isLabelDeclaration(line)) {
+                line = Syntax.useLabel(line);
+                finalCode.append(replaceLabel( line,path));
+            } else if (!Syntax.isLabel(line) && !Syntax.isLabelDeclaration(line)&& !Syntax.isEndLabel(line)) {
+                finalCode.append(line).append("  \n");
             }
         }
         return finalCode.toString();
     }
+
+    static String extractMacro(String label, Path path){
+        StringBuilder macro = new StringBuilder();
+        List<String> text = new ArrayList<>();
+        try{ text = Files.readAllLines(path);
+        }catch(Exception e) {
+            System.err.println("Error reading file");
+            System.exit(1);
+        }
+        boolean skip = true;
+        for(String line :text){
+            if(Syntax.isLabel(line) && line.contains(label)){
+                skip = false;
+            }
+            if(Syntax.isEndLabel(line) && line.contains(label)){
+                return macro.toString();
+            }
+            if(skip){
+                continue;
+            }
+            macro.append(line+ "  \n");
+
+        }
+        return macro.toString();
+    }
+   
 
     static String weave(String path) {
         List<String> text = new ArrayList<>();
@@ -38,63 +98,69 @@ public class Literate {
             System.exit(1);
         }
        
-        boolean skip = true;
+        boolean skip = false;
+        boolean inCode = false;
         StringBuilder finalText = new StringBuilder();
         
         for(String line : text) {
-           
-            if(!skip &&isLabelDeclaration(line)) {
-               finalText.append(tangle(path, line)).append("\\n");
+            System.out.println(line);
+            if(skip){
                 continue;
             }
-            if(isText(line)) {
-               skip = false;
+            if(Syntax.isText(line)){
                 continue;
             }
-            if(isTextEnd(line)){
-                skip = true;
+            if(Syntax.isTextEnd(line)){
                 continue;
             }
-            
-            if(isExample(line)) {
-                finalText.append("```java").append("\\n");
-                skip = !skip;
+            if(Syntax.isLabel(line) && !Syntax.isLabelDeclaration(line)){
+                finalText.append("# ").append(line+ "  \n");
+                
+               finalText.append("```  \n ");
                 continue;
             }
-            if(isExampleEnd(line)) {
-                finalText.append("```").append("\\n");
-                skip = !skip;
-                continue;
-            }
-            if(!skip && !isLabel(line)) {
-                finalText.append(line).append("\\n");
-            }
-            
+            finalText.append(line).append("   \n ");
+
         }
         return finalText.toString();
     }
         
         
     
+    
+   
+
+    
+}
+
+class Syntax{
     static boolean isLabel(String line) {
-        return line.startsWith("//<<") && line.endsWith(">>");
+        return line.startsWith("Label:");
+    }
+    static boolean isEndLabel(String line) {
+        return line.startsWith("EndLabel:");
     }
     static boolean isLabelDeclaration(String line) {
-        return line.startsWith("//<<<") && line.endsWith(">>>");
+        return line.startsWith("UseLabel:");
     }
     static boolean isText(String line){
-      return line.startsWith("//<text>");
+      return line.contains("//<text>");
     }
 
     static boolean isTextEnd(String line){
-      return line.startsWith("//</text>");
+      return line.contains("//</text>");
     }
     static boolean isExample(String line){
-      return line.startsWith("//<example>");
+      return line.contains("//<example>");
     }
     static boolean isExampleEnd(String line){
-      return line.startsWith("//</example>");
+      return line.contains("//</example>");
+    }
+    static String useLabel(String label){
+        return label.replace("UseLabel:","Label:");
+    }
+    static String endLabel(String label){
+        return label.replace("Label:","EndLabel:");
     }
 
-    
 }
